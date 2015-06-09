67be7f2bb91f0cd0ee8e7bab534ee6957a7ea1be
wsjerome <xiaopapasoftware@126.com>
2015-06-09 21:39:13 +0800

    commit by huangsc

--- src/main/java/com/thinkgem/jeesite/modules/inventory/entity/Room.java
+++ src/main/java/com/thinkgem/jeesite/modules/inventory/entity/Room.java
@@ -1,7 +1,118 @@
+/**
+ * Copyright &copy; 2012-2014 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
+ */
 package com.thinkgem.jeesite.modules.inventory.entity;
+
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
 
 import com.thinkgem.jeesite.common.persistence.DataEntity;
 
+/**
+ * 房间信息Entity
+ * @author huangsc
+ * @version 2015-06-09
+ */
 public class Room extends DataEntity<Room> {
+	
+	private static final long serialVersionUID = 1L;
+	private PropertyProject propertyProject;		// 物业项目
+	private Building building;		// 楼宇
+	private House house;		// 房屋号
+	private String roomNo;		// 房间号
+	private String meterNo;		// 电表号
+	private Double roomSpace;		// 房间面积
+	private String orientation;		// 朝向
+	private String structure;		// 附属结构
+	private String roomStatus;		// 房间状态
+	
+	public Room() {
+		super();
+	}
 
+	public Room(String id){
+		super(id);
+	}
+
+	@NotNull(message="物业项目不能为空")
+	public PropertyProject getPropertyProject() {
+		return propertyProject;
+	}
+
+	public void setPropertyProject(PropertyProject propertyProject) {
+		this.propertyProject = propertyProject;
+	}
+	
+	@NotNull(message="楼宇不能为空")
+	public Building getBuilding() {
+		return building;
+	}
+
+	public void setBuilding(Building building) {
+		this.building = building;
+	}
+	
+	@NotNull(message="房屋号不能为空")
+	public House getHouse() {
+		return house;
+	}
+
+	public void setHouse(House house) {
+		this.house = house;
+	}
+	
+	@Length(min=1, max=100, message="房间号长度必须介于 1 和 100 之间")
+	public String getRoomNo() {
+		return roomNo;
+	}
+
+	public void setRoomNo(String roomNo) {
+		this.roomNo = roomNo;
+	}
+	
+	@Length(min=0, max=100, message="电表号长度必须介于 0 和 100 之间")
+	public String getMeterNo() {
+		return meterNo;
+	}
+
+	public void setMeterNo(String meterNo) {
+		this.meterNo = meterNo;
+	}
+	
+	public Double getRoomSpace() {
+		return roomSpace;
+	}
+
+	public void setRoomSpace(Double roomSpace) {
+		this.roomSpace = roomSpace;
+	}
+	
+	@Length(min=0, max=64, message="朝向长度必须介于 0 和 64 之间")
+	public String getOrientation() {
+		return orientation;
+	}
+
+	public void setOrientation(String orientation) {
+		this.orientation = orientation;
+	}
+	
+	@Length(min=0, max=64, message="附属结构长度必须介于 0 和 64 之间")
+	public String getStructure() {
+		return structure;
+	}
+
+	public void setStructure(String structure) {
+		this.structure = structure;
+	}
+	
+	@Length(min=1, max=100, message="房间状态长度必须介于 1 和 100 之间")
+	public String getRoomStatus() {
+		return roomStatus;
+	}
+
+	public void setRoomStatus(String roomStatus) {
+		this.roomStatus = roomStatus;
+	}
+	
 }
---
+++ src/main/java/com/thinkgem/jeesite/modules/inventory/web/RoomController.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright &copy; 2012-2014 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
+ */
+package com.thinkgem.jeesite.modules.inventory.web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.thinkgem.jeesite.common.config.Global;
+import com.thinkgem.jeesite.common.persistence.Page;
+import com.thinkgem.jeesite.common.web.BaseController;
+import com.thinkgem.jeesite.common.utils.StringUtils;
+import com.thinkgem.jeesite.modules.inventory.entity.Room;
+import com.thinkgem.jeesite.modules.inventory.service.RoomService;
+
+/**
+ * 房间信息Controller
+ * @author huangsc
+ * @version 2015-06-09
+ */
+@Controller
+@RequestMapping(value = "${adminPath}/inventory/room")
+public class RoomController extends BaseController {
+
+	@Autowired
+	private RoomService roomService;
+	
+	@ModelAttribute
+	public Room get(@RequestParam(required=false) String id) {
+		Room entity = null;
+		if (StringUtils.isNotBlank(id)){
+			entity = roomService.get(id);
+		}
+		if (entity == null){
+			entity = new Room();
+		}
+		return entity;
+	}
+	
+	@RequiresPermissions("inventory:room:view")
+	@RequestMapping(value = {"list", ""})
+	public String list(Room room, HttpServletRequest request, HttpServletResponse response, Model model) {
+		Page<Room> page = roomService.findPage(new Page<Room>(request, response), room); 
+		model.addAttribute("page", page);
+		return "modules/inventory/roomList";
+	}
+
+	@RequiresPermissions("inventory:room:view")
+	@RequestMapping(value = "form")
+	public String form(Room room, Model model) {
+		model.addAttribute("room", room);
+		return "modules/inventory/roomForm";
+	}
+
+	@RequiresPermissions("inventory:room:edit")
+	@RequestMapping(value = "save")
+	public String save(Room room, Model model, RedirectAttributes redirectAttributes) {
+		if (!beanValidator(model, room)){
+			return form(room, model);
+		}
+		roomService.save(room);
+		addMessage(redirectAttributes, "保存房间信息成功");
+		return "redirect:"+Global.getAdminPath()+"/inventory/room/?repage";
+	}
+	
+	@RequiresPermissions("inventory:room:edit")
+	@RequestMapping(value = "delete")
+	public String delete(Room room, RedirectAttributes redirectAttributes) {
+		roomService.delete(room);
+		addMessage(redirectAttributes, "删除房间信息成功");
+		return "redirect:"+Global.getAdminPath()+"/inventory/room/?repage";
+	}
+
+}
