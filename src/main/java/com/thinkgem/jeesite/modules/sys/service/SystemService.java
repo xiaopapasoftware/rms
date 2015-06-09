22aae2b1c0c2ab2c43c3efeca3ddcb0480e4ca48
wsjerome <xiaopapasoftware@126.com>
2015-06-09 21:39:13 +0800

    微调

--- src/main/java/com/thinkgem/jeesite/modules/sys/service/SystemService.java
+++ src/main/java/com/thinkgem/jeesite/modules/sys/service/SystemService.java
@@ -399,7 +399,7 @@ public class SystemService extends BaseService implements InitializingBean {
 	public static boolean printKeyLoadMessage(){
 		StringBuilder sb = new StringBuilder();
 		sb.append("\r\n======================================================================\r\n");
-		sb.append("\r\n    欢迎使用 "+Global.getConfig("productName")+"  - Powered By http://jeesite.com\r\n");
+		sb.append("\r\n    欢迎使用 "+Global.getConfig("productName")+"  - Powered By 小爬爬软件工作室\r\n");
 		sb.append("\r\n======================================================================\r\n");
 		System.out.println(sb.toString());
 		return true;
