d706083cec518ec149940f42cca0d7c5b58a7b58
tiny_sunshine <prchsc@gmail.com>
2015-06-13 15:49:23 +0800

    commit by huangsc

---
+++ src/main/java/com/thinkgem/jeesite/modules/inventory/dao/DevicesDao.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright &copy; 2012-2014 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
+ */
+package com.thinkgem.jeesite.modules.inventory.dao;
+
+import com.thinkgem.jeesite.common.persistence.CrudDao;
+import com.thinkgem.jeesite.common.persistence.annotation.MyBatisDao;
+import com.thinkgem.jeesite.modules.inventory.entity.Devices;
+
+/**
+ * 设备信息DAO接口
+ * @author huangsc
+ * @version 2015-06-13
+ */
+@MyBatisDao
+public interface DevicesDao extends CrudDao<Devices> {
+	
+}
