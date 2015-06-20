764f90a55b9523f4af890f6bf750da53272622f1
tiny_sunshine <prchsc@gmail.com>
2015-06-20 11:45:55 +0800

    commit by huangsc

--- db/funds/funds_mysql.sql
+++ db/funds/funds_mysql.sql
@@ -3,6 +3,7 @@ drop table if exists T_TRADING_ACCOUNTS;
 drop table if exists T_PAYMENT_TRANS;
 drop table if exists T_RECEIPT;
 drop table if exists T_INVOICE;
+drop table if exists T_PAYMENT_TRADE;
 
 
 /* Create Tables */
@@ -14,7 +15,7 @@ create table T_TRADING_ACCOUNTS
    TRADE_DIRECTION      varchar(64) comment '账务交易方向',
    TRADE_MODE           varchar(64) comment '交易方式',
    TRADE_AMOUNT         float comment '交易金额',
-   TRADE_TIME           TIMESTAMP comment '交易时间',
+   TRADE_TIME           DATETIME comment '交易时间',
    PAYEE_NAME           varchar(100) comment '收款人名称',
    PAYEE_TYPE           varchar(64) comment '收款人类型',
    TRADE_STATUS         VARCHAR(64) COMMENT '账务状态',
@@ -34,8 +35,8 @@ create table T_PAYMENT_TRANS
    PAYMENT_TYPE         varchar(64) comment '款项类型',
    TRANS_ID             varchar(64) comment '交易对象',
    TRADE_DIRECTION      varchar(64) comment '交易款项方向',
-   START_DATE           TIMESTAMP comment '交易款项开始时间',
-   EXPIRED_DATE         TIMESTAMP comment '交易款项到期时间',
+   START_DATE           date comment '交易款项开始时间',
+   EXPIRED_DATE         date comment '交易款项到期时间',
    TRADE_AMOUNT         float comment '应该交易金额',
    TRANS_AMOUNT         float comment '实际交易金额',
    LAST_AMOUNT          float comment '剩余交易金额',
@@ -48,6 +49,20 @@ create table T_PAYMENT_TRANS
    DEL_FLAG             CHAR(1) DEFAULT '0' NOT NULL COMMENT '删除标记',
    primary key (ID)
 ) comment = '款项交易';
+
+create table T_PAYMENT_TRADE
+(
+   ID                   varchar(64) NOT NULL,
+   TRANS_ID             varchar(64) comment '款项ID',
+   TRADE_ID             varchar(64) comment '账务ID',
+   CREATE_BY            VARCHAR(64) COMMENT '创建者',
+   CREATE_DATE          DATETIME COMMENT '创建时间',
+   UPDATE_BY            VARCHAR(64) COMMENT '更新者',
+   UPDATE_DATE          TIMESTAMP COMMENT '更新时间',
+   REMARKS              VARCHAR(255) COMMENT '备注信息',
+   DEL_FLAG             CHAR(1) DEFAULT '0' NOT NULL COMMENT '删除标记',
+   primary key (ID)
+) comment = '款项账务关联表';
 
 create table T_RECEIPT
 (
