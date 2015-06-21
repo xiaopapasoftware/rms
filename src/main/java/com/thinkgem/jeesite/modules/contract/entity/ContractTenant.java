ce9a8d7ab91070fd933d9eb7e716888c0e381cca
tiny_sunshine <prchsc@gmail.com>
2015-06-22 00:22:25 +0800

    commit by huangsc

--- src/main/java/com/thinkgem/jeesite/modules/contract/entity/ContractTenant.java
+++ src/main/java/com/thinkgem/jeesite/modules/contract/entity/ContractTenant.java
@@ -15,8 +15,10 @@ import com.thinkgem.jeesite.common.persistence.DataEntity;
 public class ContractTenant extends DataEntity<ContractTenant> {
 	
 	private static final long serialVersionUID = 1L;
-	private String contractId;		// contract_id
-	private String tenantId;		// tenant_id
+	private String depositAgreementId;		// 定金协议
+	private String contractId;		// 出租合同
+	private String tenantId;		// 租客
+	private String leaseContractId;		// 承租合同
 	
 	public ContractTenant() {
 		super();
@@ -26,7 +28,16 @@ public class ContractTenant extends DataEntity<ContractTenant> {
 		super(id);
 	}
 
-	@Length(min=1, max=64, message="contract_id长度必须介于 1 和 64 之间")
+	@Length(min=0, max=64, message="定金协议长度必须介于 0 和 64 之间")
+	public String getDepositAgreementId() {
+		return depositAgreementId;
+	}
+
+	public void setDepositAgreementId(String depositAgreementId) {
+		this.depositAgreementId = depositAgreementId;
+	}
+	
+	@Length(min=1, max=64, message="出租合同长度必须介于 1 和 64 之间")
 	public String getContractId() {
 		return contractId;
 	}
@@ -35,13 +46,22 @@ public class ContractTenant extends DataEntity<ContractTenant> {
 		this.contractId = contractId;
 	}
 	
-	@Length(min=1, max=64, message="tenant_id长度必须介于 1 和 64 之间")
+	@Length(min=1, max=64, message="租客长度必须介于 1 和 64 之间")
 	public String getTenantId() {
 		return tenantId;
 	}
 
 	public void setTenantId(String tenantId) {
 		this.tenantId = tenantId;
+	}
+	
+	@Length(min=0, max=64, message="承租合同长度必须介于 0 和 64 之间")
+	public String getLeaseContractId() {
+		return leaseContractId;
+	}
+
+	public void setLeaseContractId(String leaseContractId) {
+		this.leaseContractId = leaseContractId;
 	}
 	
 }
