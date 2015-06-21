cb47011dc6a3dd75fd6cb211b9cd53cfe3f1e3e1
tiny_sunshine <prchsc@gmail.com>
2015-06-21 13:32:32 +0800

    commit by huangsc

---
+++ db/sys/sys_area.sql
@@ -0,0 +1,6 @@
+insert into sys_area (id, parent_id, parent_ids, name, sort, code, type, create_by, create_date, update_by, update_date, remarks, del_flag) values('1','0','0,','中国','10','100000','1','1',now(),'1',now(),NULL,'0');
+insert into sys_area (id, parent_id, parent_ids, name, sort, code, type, create_by, create_date, update_by, update_date, remarks, del_flag) values('2','1','0,1,','山东省','20','110000','2','1',now(),'1',now(),NULL,'0');
+insert into sys_area (id, parent_id, parent_ids, name, sort, code, type, create_by, create_date, update_by, update_date, remarks, del_flag) values('3','2','0,1,2,','济南市','30','110101','3','1',now(),'1',now(),NULL,'0');
+insert into sys_area (id, parent_id, parent_ids, name, sort, code, type, create_by, create_date, update_by, update_date, remarks, del_flag) values('4','3','0,1,2,3,','历城区','40','110102','4','1',now(),'1',now(),NULL,'0');
+insert into sys_area (id, parent_id, parent_ids, name, sort, code, type, create_by, create_date, update_by, update_date, remarks, del_flag) values('5','3','0,1,2,3,','历下区','50','110104','4','1',now(),'1',now(),NULL,'0');
+insert into sys_area (id, parent_id, parent_ids, name, sort, code, type, create_by, create_date, update_by, update_date, remarks, del_flag) values('6','3','0,1,2,3,','高新区','60','110105','4','1',now(),'1',now(),NULL,'0');
