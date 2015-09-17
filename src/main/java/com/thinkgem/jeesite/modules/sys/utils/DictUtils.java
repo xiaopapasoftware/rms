c042158bf64af6744d6cd746975c935e755bfe55
wsjerome <xiaopapasoftware@126.com>
2015-09-17 17:20:37 +0800

    JeeSite框架升级

--- src/main/java/com/thinkgem/jeesite/modules/sys/utils/DictUtils.java
+++ src/main/java/com/thinkgem/jeesite/modules/sys/utils/DictUtils.java
@@ -10,6 +10,7 @@ import org.apache.commons.lang3.StringUtils;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.thinkgem.jeesite.common.mapper.JsonMapper;
 import com.thinkgem.jeesite.common.utils.CacheUtils;
 import com.thinkgem.jeesite.common.utils.SpringContextHolder;
 import com.thinkgem.jeesite.modules.sys.dao.DictDao;
@@ -23,63 +24,73 @@ import com.thinkgem.jeesite.modules.sys.entity.Dict;
  */
 public class DictUtils {
 
-	private static DictDao dictDao = SpringContextHolder.getBean(DictDao.class);
+    private static DictDao dictDao = SpringContextHolder.getBean(DictDao.class);
 
-	public static final String CACHE_DICT_MAP = "dictMap";
+    public static final String CACHE_DICT_MAP = "dictMap";
 
-	public static String getDictLabel(String value, String type, String defaultValue) {
-		if (StringUtils.isNotBlank(type) && StringUtils.isNotBlank(value)) {
-			for (Dict dict : getDictList(type)) {
-				if (type.equals(dict.getType()) && value.equals(dict.getValue())) {
-					return dict.getLabel();
-				}
-			}
+    public static String getDictLabel(String value, String type, String defaultValue) {
+	if (StringUtils.isNotBlank(type) && StringUtils.isNotBlank(value)) {
+	    for (Dict dict : getDictList(type)) {
+		if (type.equals(dict.getType()) && value.equals(dict.getValue())) {
+		    return dict.getLabel();
 		}
-		return defaultValue;
+	    }
 	}
+	return defaultValue;
+    }
 
-	public static String getDictLabels(String values, String type, String defaultValue) {
-		if (StringUtils.isNotBlank(type) && StringUtils.isNotBlank(values)) {
-			List<String> valueList = Lists.newArrayList();
-			for (String value : StringUtils.split(values, ",")) {
-				valueList.add(getDictLabel(value, type, defaultValue));
-			}
-			return StringUtils.join(valueList, ",");
-		}
-		return defaultValue;
+    public static String getDictLabels(String values, String type, String defaultValue) {
+	if (StringUtils.isNotBlank(type) && StringUtils.isNotBlank(values)) {
+	    List<String> valueList = Lists.newArrayList();
+	    for (String value : StringUtils.split(values, ",")) {
+		valueList.add(getDictLabel(value, type, defaultValue));
+	    }
+	    return StringUtils.join(valueList, ",");
 	}
+	return defaultValue;
+    }
 
-	public static String getDictValue(String label, String type, String defaultLabel) {
-		if (StringUtils.isNotBlank(type) && StringUtils.isNotBlank(label)) {
-			for (Dict dict : getDictList(type)) {
-				if (type.equals(dict.getType()) && label.equals(dict.getLabel())) {
-					return dict.getValue();
-				}
-			}
+    public static String getDictValue(String label, String type, String defaultLabel) {
+	if (StringUtils.isNotBlank(type) && StringUtils.isNotBlank(label)) {
+	    for (Dict dict : getDictList(type)) {
+		if (type.equals(dict.getType()) && label.equals(dict.getLabel())) {
+		    return dict.getValue();
 		}
-		return defaultLabel;
+	    }
 	}
+	return defaultLabel;
+    }
 
-	public static List<Dict> getDictList(String type) {
-		@SuppressWarnings("unchecked")
-		Map<String, List<Dict>> dictMap = (Map<String, List<Dict>>) CacheUtils.get(CACHE_DICT_MAP);
-		if (dictMap == null) {
-			dictMap = Maps.newHashMap();
-			for (Dict dict : dictDao.findAllList(new Dict())) {
-				List<Dict> dictList = dictMap.get(dict.getType());
-				if (dictList != null) {
-					dictList.add(dict);
-				} else {
-					dictMap.put(dict.getType(), Lists.newArrayList(dict));
-				}
-			}
-			CacheUtils.put(CACHE_DICT_MAP, dictMap);
+    public static List<Dict> getDictList(String type) {
+	@SuppressWarnings("unchecked")
+	Map<String, List<Dict>> dictMap = (Map<String, List<Dict>>) CacheUtils.get(CACHE_DICT_MAP);
+	if (dictMap == null) {
+	    dictMap = Maps.newHashMap();
+	    for (Dict dict : dictDao.findAllList(new Dict())) {
+		List<Dict> dictList = dictMap.get(dict.getType());
+		if (dictList != null) {
+		    dictList.add(dict);
+		} else {
+		    dictMap.put(dict.getType(), Lists.newArrayList(dict));
 		}
-		List<Dict> dictList = dictMap.get(type);
-		if (dictList == null) {
-			dictList = Lists.newArrayList();
-		}
-		return dictList;
+	    }
+	    CacheUtils.put(CACHE_DICT_MAP, dictMap);
 	}
+	List<Dict> dictList = dictMap.get(type);
+	if (dictList == null) {
+	    dictList = Lists.newArrayList();
+	}
+	return dictList;
+    }
+
+    /**
+     * 返回字典列表（JSON）
+     * 
+     * @param type
+     * @return
+     */
+    public static String getDictListJson(String type) {
+	return JsonMapper.toJsonString(getDictList(type));
+    }
 
 }
