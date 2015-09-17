bd228224ea8bf7f858b2b623ed62cc4688698bcb
987839514@qq.com <987839514@qq.com>
2015-09-17 20:18:19 +0800

    commint

--- src/main/java/com/thinkgem/jeesite/modules/report/web/CustomerReport.java
+++ src/main/java/com/thinkgem/jeesite/modules/report/web/CustomerReport.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
