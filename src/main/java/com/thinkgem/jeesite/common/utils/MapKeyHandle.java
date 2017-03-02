a801645b0070a02ac1fae19f317a95ad79f6c8e6
wsjerome <xiaopapasoftware@126.com>
2017-03-02 23:16:24 +0800

    格式化代码

--- src/main/java/com/thinkgem/jeesite/common/utils/MapKeyHandle.java
+++ src/main/java/com/thinkgem/jeesite/common/utils/MapKeyHandle.java
@@ -1,41 +1,39 @@
 package com.thinkgem.jeesite.common.utils;
 
-import org.apache.commons.collections.MapUtils;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import org.apache.commons.collections.MapUtils;
 
 /**
  * Created by wangganggang on 17/2/26.
  */
 public class MapKeyHandle {
 
-    /** map 的 key 转成 java命名方式 **/
-    public static Map keyToJavaProperty(Map map) {
-        Map retMap = new HashMap();
-        if (map == null)
-            return null;
+  /** map 的 key 转成 java命名方式 **/
+  public static Map keyToJavaProperty(Map map) {
+    Map retMap = new HashMap();
+    if (map == null) return null;
 
-        for (Object key : map.keySet()) {
-            String newKey = StringUtils.lowerFirst(StringUtils.toCamelCase(key.toString()));
-            retMap.put(newKey, MapUtils.getObject(map, key));
-        }
-        return retMap;
+    for (Object key : map.keySet()) {
+      String newKey = StringUtils.lowerFirst(StringUtils.toCamelCase(key.toString()));
+      retMap.put(newKey, MapUtils.getObject(map, key));
     }
+    return retMap;
+  }
 
-    /** map 的 key 转成 java命名方式 **/
-    public static List<Map> keyToJavaProperty(List<Map> listMap) {
-        List<Map> newListMap = new ArrayList<Map>();
-        if (listMap == null)
-            return null;
+  /** map 的 key 转成 java命名方式 **/
+  public static List<Map> keyToJavaProperty(List<Map> listMap) {
+    List<Map> newListMap = new ArrayList<Map>();
+    if (listMap == null) return null;
 
-        for (Map<String, Object> map : listMap) {
-            newListMap.add(keyToJavaProperty(map));
-        }
-        return newListMap;
+    for (Map<String, Object> map : listMap) {
+      newListMap.add(keyToJavaProperty(map));
     }
+    return newListMap;
+  }
 
 
 }
