34682ff2851591e3132b5e2d02acf760b18ef289
wggorking@163.com <wggorking@163.com>
2017-03-04 19:25:32 +0800

    contract report ok

---
+++ src/main/java/com/thinkgem/jeesite/modules/report/service/ReportComponentSrervice.java
@@ -0,0 +1,10 @@
+package com.thinkgem.jeesite.modules.report.service;
+
+/**
+ * Created by wangganggang on 17/3/4.
+ *
+ * @author wangganggang
+ * @date 2017/03/04
+ */
+public class ReportComponentSrervice {
+}
---
+++ src/main/java/com/thinkgem/jeesite/modules/report/web/ReportComponentController.java
@@ -0,0 +1,12 @@
+package com.thinkgem.jeesite.modules.report.web;
+
+/**
+ * Created by wangganggang on 17/3/4.
+ *
+ * @author wangganggang
+ * @date 2017/03/04
+ */
+public class ReportComponent {
+
+
+}
