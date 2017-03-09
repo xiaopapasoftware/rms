ea8a4d2b4a1b1539c5d1dc76e4d13ae190b2c71f
wggorking@163.com <wggorking@163.com>
2017-03-09 18:42:05 +0800

    项目拆分

--- rms-boot/rent-due-urge/src/main/java/com/thinkgem/jeesite/Application.java
+++ rms-boot/rent-due-urge/src/main/java/com/thinkgem/jeesite/Application.java
@@ -3,10 +3,12 @@ package com.thinkgem.jeesite;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * Created by wangganggang on 2017/1/1.
  */
+@ImportResource(locations = "*.xml")
 @SpringBootApplication(scanBasePackages = {"com.thinkgem.jeesite"})
 @EnableAutoConfiguration()
 public class Application {
