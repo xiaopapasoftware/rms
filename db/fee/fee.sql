9bf15b164f0d6d71f4abae82b47e39c05c9ff3d1
wsjerome <xiaopapasoftware@126.com>
2017-03-30 15:58:27 +0800

    优化

--- db/fee/fee.sql
+++ db/fee/fee.sql
@@ -1,12 +1,13 @@
 /* Drop Tables */
 drop table if exists t_electric_fee;
+drop table if exists t_postpaid_fee;
 
 /* Create Tables */
 create table t_electric_fee
 (
    ID                   varchar(64) NOT NULL,
    RENT_CONTRACT_ID     varchar(64) comment '出租合同ID',
-   PAYMENT_TRANS_ID      varchar(64) comment '对应充值款项ID',
+   PAYMENT_TRANS_ID     varchar(64) comment '对应充值款项ID',
    CHARGE_DATE			date comment '充值时间',
    CHARGE_AMOUNT		float comment '充值金额',
    CHARGE_STATUS        varchar(64) comment '充值状态,0=充值中,1=充值成功,2=充值失败',
@@ -20,3 +21,15 @@ create table t_electric_fee
    DEL_FLAG             CHAR(1) DEFAULT '0' NOT NULL COMMENT '删除标记',
    primary key (ID)
 ) comment = '电费充值记录表';
+
+create table t_postpaid_fee
+(
+	ID 					 varchar(64) NOT NULL,
+	RENT_CONTRACT_ID     varchar(64) comment '出租合同ID',
+	PAYMENT_TRANS_ID     varchar(64) comment '对应款项ID',
+	CHARGE_DATE			 date comment '付费时间',
+ 	CHARGE_AMOUNT		 float comment '付费金额',
+	CHARGE_STATUS        varchar(64) comment '后付费用状态,1=付费成功,0=付费失败',
+    SETTLE_STATUS		 varchar(64) comment '结算状态，0=待结算；0=待结算；2=审核拒绝；3=审核通过',
+
+)comment = '后付费记录表';
