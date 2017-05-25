cfe77333cbfad7d2f230a92adf66f06e16ba15fb
wsjerome <xiaopapasoftware@126.com>
2017-05-25 14:03:02 +0800

    youhua

--- rms-modules/src/main/java/com/thinkgem/jeesite/modules/funds/web/TradingAccountsController.java
+++ rms-modules/src/main/java/com/thinkgem/jeesite/modules/funds/web/TradingAccountsController.java
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.collections.comparators.ComparableComparator;
 import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
