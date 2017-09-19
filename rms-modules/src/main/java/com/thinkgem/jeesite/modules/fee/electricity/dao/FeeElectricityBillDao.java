36854fa6e9edec12136b04d4af5ec8700d587643
wsjerome <xiaopapasoftware@126.com>
2017-09-19 23:09:19 +0800

    youhua

--- rms-modules/src/main/java/com/thinkgem/jeesite/modules/fee/electricity/dao/FeeElectricityBillDao.java
+++ rms-modules/src/main/java/com/thinkgem/jeesite/modules/fee/electricity/dao/FeeElectricityBillDao.java
@@ -1,10 +1,10 @@
 /**
- * 小爬爬工作室
- * Copyright (c) 1994-2015 All Rights Reserved.
+ * 小爬爬工作室 Copyright (c) 1994-2015 All Rights Reserved.
  */
 package com.thinkgem.jeesite.modules.fee.electricity.dao;
 
-import com.sun.tools.javac.util.List;
+import java.util.List;
+
 import com.thinkgem.jeesite.common.persistence.CrudDao;
 import com.thinkgem.jeesite.common.persistence.annotation.MyBatisDao;
 
@@ -13,15 +13,20 @@ import com.thinkgem.jeesite.modules.fee.electricity.entity.FeeElectricityBill;
 import com.thinkgem.jeesite.modules.fee.electricity.entity.vo.FeeElectricityBillVo;
 
 /**
- * <p>电费账单表实现类service</p>
- * <p>Table: fee_electricity_bill - 电费账单表</p>
+ * <p>
+ * 电费账单表实现类service
+ * </p>
+ * <p>
+ * Table: fee_electricity_bill - 电费账单表
+ * </p>
+ * 
  * @since 2017-09-18 08:24:24
  * @author generator code
-*/
+ */
 @MyBatisDao
-public interface FeeElectricityBillDao extends CrudDao<FeeElectricityBill>{
+public interface FeeElectricityBillDao extends CrudDao<FeeElectricityBill> {
 
-    List<FeeElectricityBillVo> getAllHouseFeeWithAreaAndBuildAndProperty(FeeCriteriaEntity feeCriteriaEntity);
+  List<FeeElectricityBillVo> getAllHouseFeeWithAreaAndBuildAndProperty(FeeCriteriaEntity feeCriteriaEntity);
 
-    FeeElectricityBillVo getWithProperty(String id);
+  FeeElectricityBillVo getWithProperty(String id);
 }
