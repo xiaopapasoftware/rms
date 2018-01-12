d02195e4fbdca125b38c8801122ab231f60f3ffa
wsjerome <xiaopapasoftware@126.com>
2018-01-12 10:34:29 +0800

    youhua

--- rms-modules/src/main/java/com/thinkgem/jeesite/modules/contract/web/LeaseContractController.java
+++ rms-modules/src/main/java/com/thinkgem/jeesite/modules/contract/web/LeaseContractController.java
@@ -1,14 +1,32 @@
 package com.thinkgem.jeesite.modules.contract.web;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import com.thinkgem.jeesite.common.config.Global;
 import com.thinkgem.jeesite.common.enums.ViewMessageTypeEnum;
-import com.thinkgem.jeesite.common.persistence.BaseEntity;
 import com.thinkgem.jeesite.common.persistence.Page;
 import com.thinkgem.jeesite.common.utils.StringUtils;
 import com.thinkgem.jeesite.common.web.BaseController;
 import com.thinkgem.jeesite.modules.contract.entity.AuditHis;
 import com.thinkgem.jeesite.modules.contract.entity.LeaseContract;
 import com.thinkgem.jeesite.modules.contract.entity.LeaseContractOwner;
+import com.thinkgem.jeesite.modules.contract.enums.LeaseContractAuditStatusEnum;
 import com.thinkgem.jeesite.modules.contract.service.AuditHisService;
 import com.thinkgem.jeesite.modules.contract.service.LeaseContractOwnerService;
 import com.thinkgem.jeesite.modules.contract.service.LeaseContractService;
@@ -22,22 +40,6 @@ import com.thinkgem.jeesite.modules.person.entity.Owner;
 import com.thinkgem.jeesite.modules.person.entity.Remittancer;
 import com.thinkgem.jeesite.modules.person.service.OwnerService;
 import com.thinkgem.jeesite.modules.person.service.RemittancerService;
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.shiro.authz.annotation.RequiresPermissions;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * 承租合同
@@ -93,12 +95,11 @@ public class LeaseContractController extends BaseController {
 
   private void buildIdListByOwner(LeaseContract leaseContract) {
     Owner owner = leaseContract.getOwner();
-    if (StringUtils.isNotBlank(owner.getName()) || StringUtils.isNotBlank(owner.getSocialNumber())
-            ||StringUtils.isNotBlank(owner.getCellPhone()) || StringUtils.isNotBlank(owner.getSecondCellPhone())) {
+    if (StringUtils.isNotBlank(owner.getName()) || StringUtils.isNotBlank(owner.getSocialNumber()) || StringUtils.isNotBlank(owner.getCellPhone())
+        || StringUtils.isNotBlank(owner.getSecondCellPhone())) {
       List<Owner> ownerList = ownerService.findList(owner);
       if (CollectionUtils.isNotEmpty(ownerList)) {
-        List<String> idList = leaseContractOwnerService.getContractIdListByOwnerIdList
-                (ownerList.stream().map(Owner::getId).collect(Collectors.toList()));
+        List<String> idList = leaseContractOwnerService.getContractIdListByOwnerIdList(ownerList.stream().map(Owner::getId).collect(Collectors.toList()));
         if (CollectionUtils.isNotEmpty(idList)) {
           leaseContract.setIdList(idList);
         }
@@ -180,8 +181,7 @@ public class LeaseContractController extends BaseController {
     model.addAttribute("remittancerList", remittancerList);
     List<LeaseContractOwner> contractOwnerList = leaseContractOwnerService.getListByContractId(leaseContract.getId());
     if (CollectionUtils.isNotEmpty(contractOwnerList)) {
-      leaseContract.setOwnerList(contractOwnerList.stream().map(LeaseContractOwner::getOwnerId).map(ownerService::get)
-              .collect(Collectors.toList()));
+      leaseContract.setOwnerList(contractOwnerList.stream().map(LeaseContractOwner::getOwnerId).map(ownerService::get).collect(Collectors.toList()));
     }
     return "modules/contract/leaseContractForm";
   }
@@ -196,11 +196,11 @@ public class LeaseContractController extends BaseController {
     tmpLeaseContract.setPropertyProject(leaseContract.getPropertyProject());
     tmpLeaseContract.setBuilding(leaseContract.getBuilding());
     tmpLeaseContract.setHouse(leaseContract.getHouse());
-    tmpLeaseContract.setDelFlag(BaseEntity.DEL_FLAG_NORMAL);
     List<LeaseContract> list = leaseContractService.findList(tmpLeaseContract);
-    if (null != list && list.size() > 0) {
+    if (CollectionUtils.isNotEmpty(list)) {
       tmpLeaseContract = list.get(0);
-      if ("0".equals(tmpLeaseContract.getContractStatus()) || "1".equals(tmpLeaseContract.getContractStatus())) {
+      if (LeaseContractAuditStatusEnum.TO_BE_AUDIT.getValue().equals(tmpLeaseContract.getContractStatus())
+          || LeaseContractAuditStatusEnum.PASS.getValue().equals(tmpLeaseContract.getContractStatus())) {
         if ("add".equals(leaseContract.getType())) {
           addMessage(model, ViewMessageTypeEnum.ERROR, "该房屋已签承租合同！");
           return form(leaseContract, model);
@@ -225,7 +225,7 @@ public class LeaseContractController extends BaseController {
       contractOwner.setOwnerId(owner.getId());
       leaseContractOwnerService.save(contractOwner);
     });
-    addMessage(redirectAttributes, ViewMessageTypeEnum.SUCCESS, "保存承租合同成功");
+    addMessage(redirectAttributes, ViewMessageTypeEnum.SUCCESS, "保存承租合同成功！");
     return "redirect:" + Global.getAdminPath() + "/contract/leaseContract/?repage";
   }
 
@@ -254,8 +254,8 @@ public class LeaseContractController extends BaseController {
   private String getSelectOptionResult(List<Owner> ownerList) {
     StringBuffer optionResultBuf = new StringBuffer();
     if (CollectionUtils.isNotEmpty(ownerList)) {
-      ownerList.forEach(owner -> optionResultBuf.append("<option selected value='").append(owner.getId()).append("'>").append(owner.getName()).append("-").append(owner.getSocialNumber()).append("-").append(owner.getCellPhone())
-              .append("/").append(owner.getSecondCellPhone()).append("</option>"));
+      ownerList.forEach(owner -> optionResultBuf.append("<option selected value='").append(owner.getId()).append("'>").append(owner.getName()).append("-").append(owner.getSocialNumber()).append("-")
+          .append(owner.getCellPhone()).append("/").append(owner.getSecondCellPhone()).append("</option>"));
     }
     return optionResultBuf.toString();
   }
